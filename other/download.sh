3940584849c53492264725daf4722c169ec2cdb8
ironman_iiith <aalekhj2507@gmail.com>
2015-10-09 04:04:29 -0400

    Keep going

--- other/download.sh
+++ other/download.sh
@@ -4,25 +4,28 @@ temp=.`echo -n temp | sha512sum`
 list=.`echo -n list | sha512sum`
 current=.`echo -n current | sha512sum`
 testFile=.`echo -n testFile | sha512sum`
-wget -quiet -O "$temp".html "https://w3layouts.com/"
+wget --quiet -O "$temp".html "https://w3layouts.com/"
 totalPages=`cat "$temp".html  | grep page | grep Last | grep -o "Page 1 of [0-9]*" | rev | cut -d " " -f1  | rev`
 for i in `seq 1 "$totalPages"`
 do
-	echo "Going for \"$URL\"\"$i\""
-	wget -quiet -O "$temp".html "$URL""$i" --user-agent='Mozilla/5.0 (X11; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0'
+	echo "Going for $URL$i"
+	wget --quiet -O "$temp".html "$URL""$i" --user-agent='Mozilla/5.0 (X11; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0'
 	for j in ` cat "$temp".html  | grep -B 1 'https://w3layouts.com/wp-content/uploads/2015' | egrep -o "href=\"[^\"]+"  | cut -d "\"" -f2`
 	do
-		wget -quiet -O "$testFile".html "$j"	--user-agent='Mozilla/5.0 (X11; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0'
+		echo "Downloading $j"
+		wget --quiet -O "$testFile".html "$j"	--user-agent='Mozilla/5.0 (X11; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0'
 		postURL=`cat "$testFile".html  | grep 'Download Template' | egrep -o "href=\"[^\"]+"  | cut -d "\"" -f2`
-		wget -quiet -O "$current".html "$preURL""$postURL" --user-agent='Mozilla/5.0 (X11; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0' --referer="$j"
+		wget --quiet -O "$current".html "$preURL""$postURL" --user-agent='Mozilla/5.0 (X11; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0' --referer="$j"
 		final=`cat "$current".html  | egrep -o "https://w3layouts.com/\?sdmon=download[^\"]+true" | sort | uniq  | head -1`
 		name=`echo "$final" | grep -o downloads.*zip | cut -d "/" -f2`
-		wget -quiet -O "$name" "$final" --user-agent='Mozilla/5.0 (X11; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0'
+		wget --quiet -O "$name" "$final" --user-agent='Mozilla/5.0 (X11; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0'
 		echo "\"$name\" -- \"#final\"" > "$list"
 		echo "Done for \"$name\""
 	done
-	echo "Completed page \"$i\""
+	echo "Completed page $i"
 	git add .
 	git commit -m 'Keep going'
 	git push newOne master
+	sleep 1
+	print "\033c"
 done
