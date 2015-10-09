9aa91f2451fbc11857c13a904e0c62c762c7706d
ironman_iiith <aalekhj2507@gmail.com>
2015-10-09 16:18:35 -0400

    Keep going

--- other/download.sh
+++ other/download.sh
@@ -6,7 +6,9 @@ current=.`echo -n current | sha512sum`
 testFile=.`echo -n testFile | sha512sum`
 wget --quiet -O "$temp".html "https://w3layouts.com/"
 totalPages=`cat "$temp".html  | grep page | grep Last | grep -o "Page 1 of [0-9]*" | rev | cut -d " " -f1  | rev`
-for i in `seq 1 "$totalPages"`
+echo 'From which page do you want to start'
+read start
+for i in `seq "$start" "$totalPages"`
 do
 	echo "Going for $URL$i"
 	wget --quiet -O "$temp".html "$URL""$i" --user-agent='Mozilla/5.0 (X11; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0'
@@ -19,7 +21,7 @@ do
 		final=`cat "$current".html  | egrep -o "https://w3layouts.com/\?sdmon=download[^\"]+true" | sort | uniq  | head -1`
 		name=`echo "$final" | grep -o downloads.*zip | cut -d "/" -f2`
 		wget --quiet -O "$name" "$final" --user-agent='Mozilla/5.0 (X11; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0'
-		echo "\"$name\" -- \"#final\"" > "$list"
+		echo "\"$name\" -- \"#final\"" "page number " "$i" "$j" >> "$list"
 		echo "Done for \"$name\""
 	done
 	echo "Completed page $i"
